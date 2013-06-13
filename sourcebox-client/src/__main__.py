570aac392d9c9620560b8dfcdf1f284211f1ce71
jjpic <github.20.pezen@spamgourmet.com>
2013-06-13 21:50:05 +0200

    Client: Configdatei erstellt und entsprechende Funktionalitaet zum auslesen der Konfiguration in der __main__.py hinzugefügt.

--- sourcebox-client/src/__main__.py
+++ sourcebox-client/src/__main__.py
@@ -1,7 +1,7 @@
-#
+# -*- coding: utf-8 -*-#
 # sourceboxclient – main
 #
-# @encode  UTF-8, tabwidth = , newline = LF
+# @encode  UTF-8, tabwidth = 4 , newline = LF
 # @date   03.06.13
 # @author  Johannes
 #
@@ -10,31 +10,53 @@
 '''-------------------------------------------------------------
 ' Planung/ToDo/Changelog:
 '
-' Wie können FS Events erkannt werden? ständiges überprüfen oder system calls fangen?
-' 	pypi.python.org/pypi/watchdog
-'
+' 13.06.13: Funktionalitaet zum parsen der Configdatei hinzugefügt
 '
 '
 '----------------------------------------------------------- '''
 
 
+## --------------------------------------------------------------
+## IMPORT SECTION
+## --------------------------------------------------------------
 from socket import *
 import os
 import time
-serverPort = 8080
-
-boxPath = "./" 			# path to the TumBox folder
+import ConfigParser
 
 
-# open client socket
+## --------------------------------------------------------------
+## PARSE CONFIG FILE
+## --------------------------------------------------------------
+config = ConfigParser.ConfigParser()
+try:
+	config.read("./sb_client.conf")
+except:
+	print "Error while trying to read config file. Check if 'sb_client.conf' exists in the same folder as the program file."
+
+boxPath = config.get('main', 'path')
+
+serverHostname = config.get('server', 'host')
+serverPort = config.get('server', 'port')
+
+username = config.get('user', 'name')
+password = config.get('user', 'password')
+
+
+## --------------------------------------------------------------
+## OPEN CLIENT SOCKET
+## --------------------------------------------------------------
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.settimeout(3.0)
 
 
+## --------------------------------------------------------------
+## MAIN PROGRAM
+## --------------------------------------------------------------
 # main loop:
 while(1):
-
-        # receive lock command from server
-
-
-        # wait for files system events
+	
+# receive lock command from server
+	
+# wait for files system events
+	pass
