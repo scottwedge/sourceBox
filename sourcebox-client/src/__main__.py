3068a91e3f6558b064bfb3f7643f6ce102c6eaf9
Emanuel Regnath <elgor@gmx.de>
2013-06-18 11:22:17 +0200

    Added watchdog

--- sourcebox-client/src/__main__.py
+++ sourcebox-client/src/__main__.py
@@ -11,7 +11,8 @@
 ' Planung/ToDo/Changelog:
 '
 ' 13.06.13: Funktionalitaet zum parsen der Configdatei hinzugefügt
-'
+' 18.06.13: Watchdog hinzugefügt, muss installiert werden: pip install watchdog
+' 
 '
 '----------------------------------------------------------- '''
 
@@ -23,6 +24,8 @@ from socket import *
 import os
 import time
 import ConfigParser
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
 
 
 ## --------------------------------------------------------------
@@ -50,13 +53,72 @@ clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.settimeout(3.0)
 
 
+
+## --------------------------------------------------------------
+## CREATE FILESYSTEM EVENT HANDLER
+## --------------------------------------------------------------
+class MyEventHandler(FileSystemEventHandler):
+	def __init__(self, observer):
+		self.observer = observer
+
+	def on_created(self, event):
+		if event.is_directory == True:
+			print "Directory created: ",event.src_path
+			# push changes to SVN			
+		else:
+			print "File created: ",event.src_path
+			# push changes to SVN
+        
+	def on_deleted(self, event):
+		if event.is_directory == True:
+			print "Directory deleted: ",event.src_path
+			# push changes to SVN			
+		else:
+			print "File deleted: ",event.src_path
+			# push changes to SVN
+		
+	def on_modified(self, event):
+		if event.is_directory == True:
+			print "Directory modified: ",event.src_path
+			# push changes to SVN			
+		else:
+			print "File modified: ",event.src_path
+			# push changes to SVN
+
+	def on_moved(self, event):			# also for renames!
+		if event.is_directory == True:
+			print "Directory moved from: ",event.src_path
+			print "to: ",event.dest_path
+			# push changes to SVN			
+		else:
+			print "File moved from: ",event.src_path
+			print "to: ",event.dest_path
+
+
+
+## --------------------------------------------------------------
+## START WATCHDOG
+## --------------------------------------------------------------
+observer = Observer()   
+fs_event_handler = MyEventHandler(observer)
+
+observer.schedule(fs_event_handler, boxPath, recursive=True)
+observer.start()
+
+
+
 ## --------------------------------------------------------------
 ## MAIN PROGRAM
 ## --------------------------------------------------------------
 # main loop:
-while(1):
+try:
+	while True:
+		# receive lock command from server
+		# wait for files system events
+		time.sleep(1)
+# unexpected exit
+except KeyboardInterrupt:
+	observer.stop()
+
 	
-# receive lock command from server
-	
-# wait for files system events
-	pass
+
