5290fbe0bf0a50af64bf221a266175bcf6960ae8
Emanuel Regnath <elgor@gmx.de>
2013-06-18 10:18:09 +0200

    Update watchdog.py

--- sourcebox-client/src/watchdog.py
+++ sourcebox-client/src/watchdog.py
@@ -7,6 +7,28 @@
 #
 
 
+# watchdog 0.6: alle plattformen, leider nur modified, create und delete
+# pyinotify: nur Linux, allerdings mit open event
+
+
+
+
+
+
+## --------------------------------------------------------------
+## PARSE CONFIG FILE
+## --------------------------------------------------------------
+config = ConfigParser.ConfigParser()
+try:
+    config.read("./sb_client.conf")
+except:
+	print "Error while trying to read config file. Check if 'sb_client.conf' exists in the same folder as the program file."
+
+boxPath = config.get('main', 'path')
+
+
+
+
 class Filesystem_Controller(object):
 
     def __init__(self):
