0491895fe4d4a3f6148d84a34b9f9243ad09208e
Martin Zellner <martin.zellner@gmail.com>
2013-06-21 11:42:03 +0200

    Speichern und Locken von Dateien via RCS

--- sourcebox-server/src/__main__.py
+++ sourcebox-server/src/__main__.py
@@ -16,8 +16,12 @@ class SourceBoxServer(object):
     def __init__(self):
         data = data_controller.Data_Controller('./data/')
         comm = communication_controller.Communication_Controller()
-        data.create_file('test.txt')
+        #data.create_file('test.txt')
 
+        data.lock_file('./data/test.txt')
+        data.save_file('./data/test.txt', 'Another test content')
+        print data.show_changes('./data/test.txt')
+        print data.read_file('./data/test.txt')
         print 'sourceBox server is running'
         self.command_loop()
 
--- sourcebox-server/src/data_controller.py
+++ sourcebox-server/src/data_controller.py
@@ -5,34 +5,35 @@
 # @encode  UTF-8, tabwidth = , newline = LF
 # @author  Martin
 #
-
-import subprocess
-import os.path
+import rcslib
 
 class Data_Controller(object):
 
     data_dir = ''
     def __init__(self, data_dir):
-        Data_Controller.data_dir = os.path.abspath(data_dir)
+        self.rcs = rcslib.RCS()
         print 'Created Data_Controller in dir ' + data_dir
+    def read_file(self, file_name):
+        current_file = open(file_name, 'r')
+        return current_file.read()
 
     def lock_file(self, file_name):
-        arguments = ['co', '-l', file_name]
-        process = subprocess.Popen(arguments)
-        process.wait()
+        self.rcs.checkout(file_name, True)
 
-    def unlock_file(self):
-        pass
+    def unlock_file(self, file_name):
+        self.rcs.unlock(file_name)
 
-    def delete_file(self):
-        pass
+    def delete_file(self, file_name):
+        self.rcs._remove(file_name)
 
     def create_file(self, file_name):
         print 'Create file ' + file_name
-        arguments = ['ci', '-u', '-m\'Auto Checkin.\'', file_name]
-        process = subprocess.Popen(arguments)
-        process.wait()
+        self.rcs.checkin(file_name, 'Created file ' + file_name)
 
+    def save_file(self, file_name, content):
+        current_file = open(file_name, 'w')
+        current_file.write(content)
+        self.rcs.checkin(file_name, 'Changed by user')
 
-    def save_file():
-        pass
+    def show_changes(self, file_name):
+        return self.rcs.log(file_name)
