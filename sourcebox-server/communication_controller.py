6266d88a8cc57e5cc3c1cc1a9564ff5d4baac3e3
Paul <Paul.Subkowski@gmx.de>
2013-06-28 15:48:18 +0200

    com_controller auf der serverseite ausgebaut
    bitte auf die parameter beim aufruf der funktionen auchten

--- sourcebox-server/communication_controller.py
+++ sourcebox-server/communication_controller.py
@@ -6,18 +6,24 @@
 # @author  Paul
 #
 
-
-# push functions (requests)
-
-
 class Communication_Controller(object):
     computer_name = None
     import socket
     import os.path
 
+
+    COMMAND_SENDUPDATEFILE = 'SendUpdateFile'
+    COMMAND_SENDCREATEFILE = 'SendCreateFile'
+    COMMAND_SENDDELETEFILE = 'SendDeleteFile'
+    COMMAND_GETCREATEFILE = 'GetCreateFile'
+    COMMAND_GETLOCKFILE = 'GetLockFile'
+    COMMAND_GETMODIFYFILE = 'GetModifyFile'
+    COMMAND_GETUNLOCKFILE = 'GetUnlockFile'
+    COMMAND_GETDELETEFILE = 'GetDeleteFile'
     COMMAND_GETVERSION = 'GetVersion'
     COMMAND_GETFILESIZE = 'GetFileSize'
     COMMAND_GETFILE = 'GetFile'
+    
     VERSION = "ver1.0"
 
     def __init__(self, parent):
@@ -26,50 +32,161 @@ class Communication_Controller(object):
         sock.bind(('', 50000))
         sock.listen(1)
         s,a = sock.accept()
+
         print 'Server Created Communication_Controller'
         self.parent = parent
 
-	# Updates the file on the client
-    def send_update_file(self, path):
-        pass
+        while True:
+            data = s.recv(1024)
+            cmd = data[:data.find(' ')]
 
-    def send_create_file(self, path, content):
-        pass
+            if cmd == COMMAND_GETCREATEFILE:
+                cmd, path, name, size = data.split()
+                s.sendall('ok')
+                content = ''
+                while size > len(content):
+                    data = sock.recv(1024)
+                    if not data: 
+                        break
+                    content += data
+                s.sendall('ok')
+                answer = _get_create_file(self, path, name, content)
+                if answer == true:
+                    s.sendall('filecreated')
+                else:
+                    s.sendall('createdfail')
 
-    def send_delete_file(self, path):
-        pass
+            if cmd == COMMAND_GETLOCKFILE:
+                cmd, path, name = data.split()
+                s.sendall('ok')
+                answer = _get_lock_file(self, path, name)
+                if answer == true:
+                    s.sendall('filelocked_')
+                else:
+                    s.sendall('lockedfail')
+            
+            if cmd == COMMAND_GETMODIFYFILE:
+                cmd, path, name, size = data.split()
+                s.sendall('ok')
+                content = ''
+                while size > len(content):
+                    data = sock.recv(1024)
+                    if not data: 
+                        break
+                    content += data
+                s.sendall('ok')
+                answer = _get_modify_file(self, path, name, content)
+                if answer == true:
+                    s.sendall('filemodified')
+                else:
+                    s.sendall('modifiedfail')
 
-    # pull functions (response)
-    def get_create_file(self, path):
-        self.parent.create_file(path)
+            if cmd == COMMAND_GETUNLOCKFILE:
+                cmd, path, name = data.split()
+                s.sendall('ok')
+                answer = _get_unlock_file(self, path, name)
+                if answer == true:
+                    s.sendall('fileunlocked')
+                else:
+                    s.sendall('unlockedfail')
+
+            if cmd == COMMAND_GETDELETEFILE:
+                cmd, path, name = data.split()
+                s.sendall('ok')
+                answer = _get_delete_file(self, path, name)
+                if answer == true:
+                    s.sendall('filedeleted')
+                else:
+                    s.sendall('deletedfail')
+
+
+
+            #if cmd == COMMAND_GETFILE:
+            #            cmd, fileName = data.split()
+            #            filePathBase = os.path.dirname(__file__)
+            #            filePath = os.path.join(filePathBase, fileName)
+            #            print "filepath", filePath
+            #            s.sendall('ok')
+            #            with open(filePath, 'rb') as f:
+            #                data = f.read()
+            #            s.sendall('%16d' % len(data))
+            #            s.sendall(data)
+            #            s.recv(2)
+
+            if cmd == COMMAND_GETVERSION: 
+                s.sendall(VERSION)
+    
+            if cmd == COMMAND_GETFILESIZE: 
+                cmd, fileName = data.split()
+                filePathBase = os.path.dirname(__file__)
+                filePath = os.path.join(filePathBase, fileName)
+                size = GetFileSizeDirect(filePath)
+                s.sendall(str(size))
+                
         
 
-    def get_lock_file(self, path):
-        self.parent.lock_file(path)
+    # Server initiates the action and send a command to client
+    def send_update_file(self, path, name, size, content):
+        s.sendall(COMMAND_SENDUPDATEFILE + ' ' + path + ' ' + name + ' ' + size)
+        r = s.recv(2)
+        s.sendall(content)
+        answer = s.recv(11)
+        if answer == 'fileupdated':
+            return true
+        else:
+            return false
+
+    def send_create_file(self, path, name, size, content):
+        s.sendall(COMMAND_SENDCREATEFILE + ' ' + path + ' ' + name + ' ' + size)
+        r = s.recv(2)
+        s.sendall(content)
+        answer = s.recv(11)
+        if answer == 'filecreated':
+            return true
+        else:
+            return false
+
+    def send_delete_file(self, path, name):
+        s.sendall(COMMAND_SENDDELETEFILE + ' ' + path + ' ' + name)
+        r = s.recv(2)
+        answer = s.recv(11)
+        if answer == 'filedeleted':
+            return true
+        else:
+            return false
+
+    # Client initiated the action and sent a command to Server
+
+    def _get_create_file(self, path, name, content):
+        answer = self.parent.create_file(path, name, content)
+        return answer
+
+    def _get_lock_file(self, path, name):
+        answer = self.parent.lock_file(path, name)
+        return answer
         
 
-    def get_modify_file(self, path):
-        self.parent.modify_file(path)
+    def _get_modify_file(self, path, name, content):
+        answer = self.parent.modify_file(path, name, content)
+        return answer
+
+    def _get_unlock_file(self, path, name):
+        answer = self.parent.unlock_file(path, name)
+        return answer
         
 
-    def get_unlock_file(self, path):
-        self.parent.unlock_file(path)
-        
-
-    def get_delete_file(self, path):
-        self.parent.delete_file(path)
-        
+    def _get_delete_file(self, path, name):
+        answer = self.parent.delete_file(path, name)
+        return answer
 
     # Internal functions
-    def _create_socket(self):
-        pass
-
-    def PressEnterKey():
+   
+    def _PressEnterKey():
         print "Please, Press Enter Key ..."
         data = sys.stdin.readline()
         return 0
 
-    def GetFileSizeDirect(filePath):
+    def _GetFileSizeDirect(filePath):
         if os.path.exists(filePath):
             return os.path.getsize(filePath)
         else:
--- sourcebox-server/sourcebox_server.py
+++ sourcebox-server/sourcebox_server.py
@@ -38,27 +38,32 @@ class SourceBoxServer(object):
 
     ## Is called when a client creates a file.
     # Creates the file on all clients and in the data backend
-    def create_file(self, file_name):
+    def create_file(self, path, name, content):
         self.data.create_file(file_name)
+        # return true if successfully created
     
     ## Is called when a client locks a file.
     # Locks the file in the backend
     # @param file_name
-    def lock_file(self, file_name):
+    def lock_file(self, path, file_name):
         self.data.lock_file(file_name)
+        # return true if successfully locked
 
     ## Is called when a client unlocks a file.
     # Unlocks the file in the backend
     # @param file_name
-    def unlock_file(self, file_name):
+    def unlock_file(self, path, name):
         self.data.unlock_file(file_name)
+        # return true if successfully unlocked
 
     ## Is called when a client changes a file.
     # Updates the file on all clients and in the data backend
-    def modify_file(self):
+    def modify_file(self, path, name, content):
+        # return true if successfully modified
         pass
 
     ## Is called when a client deletes a file.
     # Deletes the file on all clients and in the data backend
-    def delete_file(self):
+    def delete_file(self, path, name):
+        # return true if successfully deleted
         pass
