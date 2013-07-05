d8fb283d82bf58b783a1db7633e3824c9ababe2b
Elgor <emanuel.regnath@tum.de>
2013-07-05 13:09:41 +0200

    relative and absolute path correction

--- sourcebox-client/filesystem_controller.py
+++ sourcebox-client/filesystem_controller.py
@@ -26,7 +26,7 @@ class Filesystem_Controller(FileSystemEventHandler):
     # @param boxPath path to the directory that will be observed  
     # @author Emanuel Regnath
     def __init__(self, client, boxPath):
-        self.boxPath = boxPath										# path of the observed directory (where fs-events will be detected)
+        self.boxPath = os.abspath(boxPath)							# absolute path of the observed directory (where fs-events will be detected)
         self.client = client										# object pointer to the parent class (client) 
         self.observer = Observer()									# create observer
         self.observer.schedule(self, boxPath, recursive=True)		# attach path to observer (recursive: also observe sub-directories)
@@ -43,14 +43,14 @@ class Filesystem_Controller(FileSystemEventHandler):
     #==========================================================================
 
     # locks a file
-    # @param path path of the file
+    # @param path path of the file relative to boxPath
     # @author Emanuel Regnath
     def lockFile(self, path):
         try:
-            file_name = os.path.basename(path)	
-            self.locked_files.append(file_name)						# new entry in locked_files
+            self.locked_files.append(path)						# new entry in locked_files
 
             self.ignoreModify.append(path)							# ignore modify-event raised by chmod
+            path = os.path.join(self.boxPath, path)					# expand to absolute path
             os.chmod(path, 0o000)									# set file permissions: no read, no write, no exec
             print "File locked ", path 								# log
         except Exception, e:
@@ -58,13 +58,13 @@ class Filesystem_Controller(FileSystemEventHandler):
             print "because ", e										# print exception
 
     # unlocks a file
-    # @param path path of the file
+    # @param path path of the file relative to boxPath
     # @author Emanuel Regnath
     def unlockFile(self, path):
         try:
             self.client.comm.send_unlock_file(path)					# send unlock command from client to server
-            file_name = os.path.basename(path)
-            self.locked_files.remove(file_name)						# remove file from locked list
+
+            self.locked_files.remove(path)						# remove file from locked list
             self.client.gui.locked_files.set(						# new entry in GUI notification
             	'\n'.join(self.locked_files))		
 
@@ -76,61 +76,76 @@ class Filesystem_Controller(FileSystemEventHandler):
             print "because ", e
 
     # wait a certain time (new thread) until path is auto-unlocked
-    # @param path path of the file
+    # @param path path of the file relative to boxPath
     # @param time time to wait in seconds
     # @author Emanuel Regnath
     def setLockTimer(self, path, time):
         Timer(time, self.unlockFile, (path,)).start()				# start new thread with timer
 
     # reads a file
-    # @param path path of the file
+    # @param path path of the file relative to boxPath
     # @author Emanuel Regnath
     # @returns the content of the file
     def readFile(self, path):
+    	path = os.path.join(self.boxPath, path)						# expand to absolute path
         return open(path, 'r').read()
 
     # overwrites a file
-    # @param path path of the file
+    # @param path path of the file relative to boxPath
     # @param content content of the file   
     # @author Emanuel Regnath
     def writeFile(self, path, content):
         self.ignoreModify.append(path)								# ignore modify-event triggered by .write
-        open(path, 'w').write(content)								# write content to file
+        path = os.path.join(self.boxPath, path)						# expand to absolute path
+        if os.access(path, os.W_OK):
+        	open(path, 'w').write(content)							# write content to file
+        else:
+       		fileMod = os.stat(path).st_mode & 0777					# get fileMod
+        	os.chmod(path, 0o666)									# set file permissions: read and write
+        	open(path, 'w').write(content)							# write content to file
+        	os.chmod(path, fileMod)
+        
 
     # created File
-    # @param path path of the file
+    # @param path path of the file relative to boxPath
     # @author Emanuel Regnath 
     def createFile(self, path):
         self.ignoreCreate.append(path)								# ignore create-event triggered by .close
+        path = os.path.join(self.boxPath, path)						# expand to absolute path
         open(path, 'a').close()										# create file
 
     # create Directory
-    # @param path path of the directory
+    # @param path path of the directory relative to boxPath
     # @author Emanuel Regnath 
     def createDir(self, path):
         self.ignoreCreate.append(path)								# ignore create-event triggered by os.makedirs
+        path = os.path.join(self.boxPath, path)						# expand to absolute path
         os.makedirs(path)											# create directory
 
     # delete File
-    # @param path path of the file
+    # @param path path of the file relative to boxPath
     # @author Emanuel Regnath 
     def deleteFile(self, path):
         self.ignoreDelete.append(path)								# ignore delete-event triggered by os.remove
+        path = os.path.join(self.boxPath, path)						# expand to absolute path
         os.remove(path)												# delete file
 
     # delete directory
-    # @param path path of the directory
+    # @param path path of the directory relative to boxPath
     # @author Emanuel Regnath 
     def deleteDir(self, path):
         self.ignoreDelete.append(path)								# ignore delete-event triggered by os.removedirs
+        path = os.path.join(self.boxPath, path)						# expand to absolute path
         os.removedirs(path)											# delete directory
 
     # moves or renames a File OR Directory
-    # @param srcPath old path of the file/directory
-    # @param dstPath new path of the file/directory    
+    # @param srcPath old path of the file or directory relative to boxPath
+    # @param dstPath new path of the file or directory relative to boxPath  
     # @author Emanuel Regnath 
     def moveFileDir(self, srcPath, dstPath):
         self.ignoreMove.append(srcPath)								# ignore move-event triggered by os.renames
+        srcPath = os.path.join(self.boxPath, srcPath)				# expand to absolute path
+        dstPath = os.path.join(self.boxPath, dstPath)				# expand to absolute path
         # TODO: test if dest is in lokal folder?					
         os.renames(srcPath, dstPath)								# move file or directory
 
@@ -139,6 +154,7 @@ class Filesystem_Controller(FileSystemEventHandler):
     # @author Emanuel Regnath
     # @returns the size of the file
     def getSize(self, path):
+    	path = os.path.join(self.boxPath, path)						# expand to absolute path
         return os.path.getsize(path)
 
 
@@ -151,78 +167,78 @@ class Filesystem_Controller(FileSystemEventHandler):
     # @param event object representing the file system event
     # @author Emanuel Regnath 
     def on_created(self, event):
-        if event.src_path in self.ignoreCreate:
-            self.ignoreCreate.remove(event.src_path)
+    	src_path = os.path.relpath(event.src_path, self.boxPath)	# reduce to path relative to boxPath
+        if src_path in self.ignoreCreate:
+            self.ignoreCreate.remove(src_path)
         else:
             if event.is_directory == True:							# if event was triggered by a directory
-                print "Directory created: ", event.src_path
+                print "Directory created: ", src_path
                 # push changes to SVN
             else:
-                print "File created: ", event.src_path
-                file_name = os.path.basename(event.src_path)
-                current_file = open(event.src_path)
-                file_content = current_file.read()
+                print "File created: ", src_path
+                content = open(src_path).read()
                 self.client.comm.send_create_file(
-                    file_name, len(file_content), file_content)
+                    src_path, len(content), content)
 
 
     # triggered if a file or directory was deleted
     # @param event object representing the file system event
     # @author Emanuel Regnath 
     def on_deleted(self, event):
-        if event.src_path in self.ignoreDelete:
-            self.ignoreDelete.remove(event.src_path)
+    	src_path = os.path.relpath(event.src_path, self.boxPath)	# reduce to path relative to boxPath
+        if src_path in self.ignoreDelete:
+            self.ignoreDelete.remove(src_path)
         else:
             if event.is_directory == True:							# if event was triggered by a directory
-                print "Directory deleted: ", event.src_path
-                #
+                print "Directory deleted: ", src_path
+                # push changes to SVN
             else:
-                print "File deleted: ", event.src_path
-                file_name = os.path.basename(event.src_path)
-                self.client.comm.send_delete_file(file_name)		# send delete_file to server
+                print "File deleted: ", src_path
+                self.client.comm.send_delete_file(src_path)			# send delete_file to server
 
 
     # triggered if a file or directory was modified
     # @param event object representing the file system event
     # @author Emanuel Regnath 
     def on_modified(self, event):
-        if event.src_path in self.ignoreModify:
-            self.ignoreModify.remove(event.src_path)
+    	src_path = os.path.relpath(event.src_path, self.boxPath)	# reduce to path relative to boxPath
+        if src_path in self.ignoreModify:
+            self.ignoreModify.remove(src_path)
         else:
             if event.is_directory == True:							# if event was triggered by a directory
-                print "Directory modified: ", event.src_path
+                print "Directory modified: ", src_path
                 # push changes to SVN
             else:
-                print "File modified: ", event.src_path				# log
+                print "File modified: ", src_path					# log
                 # lock file:
-                file_name = os.path.basename(event.src_path)
-
-                self.client.comm.send_lock_file(file_name)
-                self.locked_files.append(file_name)
+                self.client.comm.send_lock_file(src_path)
+                self.locked_files.append(src_path)
                 self.client.gui.locked_files.set(
                     '\n'.join(self.locked_files))
 
-                self.setLockTimer(file_name, self.lockTime)			# start lockTimer for auto-unlock
+                self.setLockTimer(src_path, self.lockTime)			# start lockTimer for auto-unlock
                 # push changes to SVN:
-                content = self.readFile(event.src_path)				# read file content
+                content = self.readFile(src_path)					# read file content
                 self.client.comm.send_modify_file(					# send modify_file to server
-                    file_name, len(content), content)
+                    src_path, len(content), content)
 
 
     # triggered if a file or directory was moved or renamed
     # @param event object representing the file system event
     # @author Emanuel Regnath 
-    def on_moved(self, event):			
-        if event.src_path in self.ignoreMove:
-            self.ignoreMove.remove(event.src_path)
+    def on_moved(self, event):
+    	src_path = os.path.relpath(event.src_path, self.boxPath)	# reduce to path relative to boxPath
+    	dest_path = os.path.relpath(event.dest_path, self.boxPath)	# reduce to path relative to boxPath	
+        if src_path in self.ignoreMove:
+            self.ignoreMove.remove(src_path)
         else:
             if event.is_directory == True:							# if event was triggered by a directory
-                print "Directory moved from: ", event.src_path
-                print "to: ", event.dest_path
+                print "Directory moved from: ", src_path
+                print "to: ", dest_path
                 # push changes to SVN
             else:
-                print "File moved from: ", event.src_path
-                print "to: ", event.dest_path
-                if event.src_path == None:
+                print "File moved from: ", src_path
+                print "to: ", dest_path
+                if src_path == None:
                     # same as create
                     pass
